d68147fe421ef23e4b3903d9fac62eb0020b0119
zasaeed <zainabsaeed2004@gmail.com>
2024-06-30 01:39:03 +0300

    minor issues, check notes

--- handlers/LocalIP.go
+++ handlers/LocalIP.go
@@ -3,20 +3,13 @@ package handlers
 import (
 	"net"
 	"log"
-    "os"
-    "fmt"
 )
 
 
 
 func GetLocalIP() string {
     var localAddress string
-
-    //Usage: ./TCPChat $port
-    if len(os.Args) > 2 {
-        fmt.Println(Usagemessage)
-        os.Exit(1)
-    }
+    
     //use the local address of the machine running the program
     conn, err := net.Dial("udp", "8.8.8.8:80")
     if err != nil {
--- server/TCPChat.go
+++ server/TCPChat.go
@@ -4,17 +4,25 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"os"
 
 	n "topchat/handlers"
 )
 
 func main() {
 	port := n.GetPort()
+	//this gets the external ip address of the machine and prints it to make it easier to connect to the server
+	//but we can go extra steps and use "ip a" in terminal to get the ip
 	ip := n.GetLocalIP()
 
-    
+      //Usage: ./TCPChat $port
+	if len(os.Args) > 2 {
+        fmt.Println(n.Usagemessage)
+        os.Exit(1)
+    }
 
 	// create server to listen for connections
+	//"0.0.0.0" will allow incoming connections from any network interface, local or external
 	ln, err := net.Listen("tcp", "0.0.0.0:"+port)
 	if err != nil {
 		log.Fatalf("Error setting up listener: %v\n", err)
